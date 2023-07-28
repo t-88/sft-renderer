4450c1936604d5f74e832f416e359ab31be98a2e
t-88 <temptaki5002@gmail.com>
2023-07-28 18:59:48 +0100

    drawing lines

--- main.c
+++ main.c
@@ -9,6 +9,10 @@
                             .a = ((color) >> 8 * 0) & 0xFF  \
                          }
 
+#define canvas_draw_pixel(canvas,x,y,c) canvas.pixels[(x) + (y) * canvas.w].color = canvas_hex_to_color((c)) 
+#define SWAP(type,a,b) { type t = a; a = b; b = t;  } 
+
+
 
 
 
@@ -72,21 +76,21 @@ Canvas canvas_new(int w, int h) {
     
     return canvas;
 }
-void canvas_destroy(Canvas* canvas) {
-    free(canvas->pixels);
+void canvas_destroy(Canvas canvas) {
+    free(canvas.pixels);
 }
 
 Color canvas_hex_to_color(Int32 color) {
-    if(color > 0xFFFFFF) {
-        return HexToColor(color);
+    if(color <= 0xFFFFFF) {
+        color <<= 8;
     } 
-    return HexToColor((color << 8));
+    return HexToColor(color);
 }
-void canvas_clear(Canvas* canvas,Int32 color) {
+void canvas_clear(Canvas canvas,Int32 color) {
     Color c = canvas_hex_to_color(color); 
-    for (size_t y = 0; y < canvas->h; y++)
-        for (size_t x = 0; x < canvas->w; x++) {
-            canvas->pixels[x + y * canvas->w].color = c;
+    for (size_t y = 0; y < canvas.h; y++)
+        for (size_t x = 0; x < canvas.w; x++) {
+            canvas.pixels[x + y * canvas.w].color = c;
         }
 }
 
@@ -110,11 +114,10 @@ int canvas_to_ppm(Canvas canvas,const char* file_name) {
     fwrite("\n",1,1,f);
 
 
-    for (size_t y = 0; y < canvas.h; y++) 
-        for (size_t x = 0; x < canvas.w; x++) {
-            String r = string_from_int(canvas.pixels[x+y*canvas.w].color.r);            
-            String g = string_from_int(canvas.pixels[x+y*canvas.w].color.g);            
-            String b = string_from_int(canvas.pixels[x+y*canvas.w].color.b);            
+    for (size_t i = 0; i < canvas.h * canvas.w; i++) {
+            String r = string_from_int(canvas.pixels[i].color.r);            
+            String g = string_from_int(canvas.pixels[i].color.g);            
+            String b = string_from_int(canvas.pixels[i].color.b);            
 
             fwrite(r.val,1,r.count,f);
             fwrite(" ",1,1,f);
@@ -133,11 +136,42 @@ int canvas_to_ppm(Canvas canvas,const char* file_name) {
     fclose(f);  
 }
 
+
+void canvas_draw_line(Canvas canvas,int x1,int y1, int x2,int y2,Int32 c) {
+
+    double a = 0;
+    if(x1 != x2) {
+        if(x1 > x2) {
+            SWAP(int,y1,y2);
+            SWAP(int,x1,x2);
+        }
+        a = (double)(y1 - y2)/(x1 - x2); 
+        for (size_t x = x1; x < x2; x++) {
+            canvas_draw_pixel(canvas,x,(int) (a * x + y1),c);
+        }
+    } else {
+        if(y1 > y2) {
+            SWAP(int,y1,y2);
+        }
+        for (size_t y = y1; y < y2; y++) {
+            canvas_draw_pixel(canvas,x1,y,c);
+        }
+    }
+
+}
+
+
 int main() {
     Canvas canvas = canvas_new(100,100);
     
-    canvas_clear(&canvas,0xFFFF00);
+    canvas_clear(canvas,0x0000000);
+    canvas_draw_line(canvas,canvas.w,canvas.h,0,0,0xFF00FF);
+    canvas_draw_line(canvas,canvas.w,0,0,canvas.h,0xFFFFFF);
+    canvas_draw_line(canvas,canvas.w / 2,0,canvas.w/2,canvas.h,0x0000FF);
+    canvas_draw_line(canvas,0,canvas.h / 2,canvas.w/2,canvas.h / 2,0x555555);
+
     canvas_to_ppm(canvas,"output.ppm");
 
-    canvas_destroy(&canvas);
+
+    canvas_destroy(canvas);
 }

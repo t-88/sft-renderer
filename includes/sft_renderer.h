9404b4d8b62cc3e0897378c9fbacd443653b60dd
t-88 <temptaki5002@gmail.com>
2023-07-29 21:16:53 +0100

    drawing traingles and circls

--- includes/sft_renderer.h
+++ includes/sft_renderer.h
@@ -155,26 +155,45 @@ void canvas_draw_pixel(sftr_Canvas canvas,int x,int y,sftr_Int32 c) {
     }
 } 
 
+
+//TODO: Understand this shit plz
 void canvas_draw_line(sftr_Canvas canvas,int x1,int y1, int x2,int y2,sftr_Int32 c) {
-    double a = 0;
-    if(x1 != x2) {
-        if(x1 > x2) {
-            sftr_SWAP(int,x1,x2);
-            sftr_SWAP(int,y1,y2);
+    // bresenham algorithm
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int slope = dy > dx;
+
+    if (slope) {
+        sftr_SWAP(int,x1, y1);
+        sftr_SWAP(int,x2, y2);
+    }
+
+    if (x1 > x2) {
+        sftr_SWAP(int,x1, x2);
+        sftr_SWAP(int,y1, y2);
+    }
+
+    dx = abs(x2 - x1);
+    dy = abs(y2 - y1);
+    int err = dx / 2;
+    int y = y1;
+    int ystep = y1 < y2 ? 1 : -1;
+
+    for (int x = x1; x <= x2; x++) {
+        if(slope) {
+            canvas_draw_pixel(canvas,y,x,c);
+        } else {
+            canvas_draw_pixel(canvas,x,y,c);
         }
-        a = (double)(y1 - y2)/(x1 - x2); 
-        double b = y1 - a * x1;
-        for (size_t x = x1; x < x2; x++) {
-            canvas_draw_pixel(canvas,x,(int) (a * x + b),c);
-        }
-    } else {
-        if(y1 > y2) {
-            sftr_SWAP(int,y1,y2);
-        }
-        for (size_t y = y1; y < y2; y++) {
-            canvas_draw_pixel(canvas,x1,y,c);
+        err -= dy;
+
+        if (err < 0) {
+            y += ystep;
+            err += dx;
         }
     }
+    
+
 
 }
 
@@ -203,6 +222,65 @@ void canvas_draw_rect(sftr_Canvas canvas, int x, int y, int w , int h, sftr_Int3
 
 }
 
+
+
+
+void canvas_draw_circle(sftr_Canvas canvas,int x,int y,int r , sftr_Int32 color) {
+    if(r <= 0) {
+        assert(0 && "[Error] canvas_draw_circle: cant have zero or nigative radius");
+        return;
+    }
+    for (int j = -r; j < r; j++) {
+        for (int i =  -r; i < r; i++) { 
+            if(i*i + j*j <= r*r) {
+                canvas_draw_pixel(canvas,x+i,y+j,color);
+            }
+        }
+    }
+    
+}
+
+// TODO: make better
+void canvas_draw_traingle(sftr_Canvas canvas,int x1,int y1,int x2,int y2,int x3,int y3) {
+    // top y
+    if(y1 > y2) {sftr_SWAP(int,y2,y1);sftr_SWAP(int,x2,x1);}
+    if(y1 > y3) {sftr_SWAP(int,y1,y3);sftr_SWAP(int,x1,x3);}
+    // min y
+    if(y2 > y3) {sftr_SWAP(int,y2,y3);sftr_SWAP(int,x2,x3);}
+
+
+    float d12 = (float)(x1 - x2) / (y1 - y2);
+    float d13 = (float)(x1 - x3) / (y1 - y3);
+
+    float b12 = y1 - x1 / d12;
+    float b13 = y1 - x1 / d13;
+
+    for (int y = y1; y < y2; y++) {
+        int x12 = x1;
+        if(d12 != 0) {
+            x12 = (y - b12) * d12;
+        }
+        int x13 = x1;
+        if(d13 != 0) {
+            x13 = (y - b13) * d13;
+        }
+        canvas_draw_line(canvas,x12,y,x13,y,0xFFFFFF);
+    }
+
+    float d32 = (float)(x3 - x2) / (y3 - y2);
+    float b32 = y2 -  x2 / d32; 
+    for (int y = y2; y < y3; y++) {
+        int x32 = x1;
+        if(d32 != 0) {
+            x32 = (y - b32) * d32;
+        }
+        int x13 = x1;
+        if(d13 != 0) {
+            x13 = (y - b13) * d13;
+        }
+        canvas_draw_line(canvas,x13,y,x32,y,0xFFFFFF);
+    }
+}
 
 
 #endif
